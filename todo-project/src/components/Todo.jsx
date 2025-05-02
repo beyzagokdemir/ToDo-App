5a1fb9cdd3f1a8b3177ca0283197fb48bf47ba03
beyzagokdemir <beyzagokdemir45@gmail.com>
2025-05-02 23:22:16 +0300

    Add feature to update todo items

--- todo-project/src/components/Todo.jsx
+++ todo-project/src/components/Todo.jsx
@@ -1,28 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosRemoveCircle } from "react-icons/io";
-import { CiEdit } from "react-icons/ci";
-function Todo({ todo, onremoveTodo }) {
-  const { id, content } = todo;
+import { FaEdit } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
+import "../App.css";
+
+function Todo({ todo, onRemoveTodo, onupdateTodo }) {
+  const { id, content } = todo; // destructing
+
+  const [editable, setEditable] = useState(false);
+  const [newTodo, setNewTodo] = useState(content);
 
   const removeTodo = () => {
-    onremoveTodo(id);
+    onRemoveTodo(id);
+  };
+
+  const updateTodo = () => {
+    const request = {
+      id: id,
+      content: newTodo,
+    };
+    onupdateTodo(request);
+    setEditable(false);
   };
 
   return (
     <div
-      className="todo"
       style={{
         display: "flex",
         flexDirection: "row",
-        justifyContent: "space-between",
         alignItems: "center",
+        justifyContent: "space-between",
+        border: "1px solid lightgrey",
+        padding: "10px",
         marginTop: "10px",
       }}
     >
-      <div>{content}</div>
       <div>
-        <IoIosRemoveCircle onClick={removeTodo} className="todo-icons" />
-        <CiEdit className="todo-icons" />
+        {editable ? (
+          <input
+            style={{ width: "380px" }}
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            className="todo-input"
+            type="text"
+          />
+        ) : (
+          content
+        )}
+      </div>
+      <div>
+        <IoIosRemoveCircle className="todo-icons" onClick={removeTodo} />
+
+        {editable ? (
+          <FaCheck onClick={updateTodo} className="todo-icons" />
+        ) : (
+          <FaEdit className="todo-icons" onClick={() => setEditable(true)} />
+        )}
       </div>
     </div>
   );
